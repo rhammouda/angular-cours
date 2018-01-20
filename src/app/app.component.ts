6e5a5f151e7f27fba4736f82b06145c8b59c59cf
Your Name <you@example.com>
2018-01-20 01:21:56 +0100

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Article} from './article/article.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+
+  public articles:Article[];
+
+  constructor(){
+    this.articles = [];
+  }
+  addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
+  console.log(`Adding article title: ${title.value} and link: ${link.value}`);
+  this.articles.push(new Article(title.value,link.value));
+
+  title.value = '';
+  link.value = '';
+
+  return false;
+  }
+
+  sortedArticles(): Article[] {
+    return this.articles.sort((a: Article, b: Article) => b.votes - a.votes);
+  }
 }
